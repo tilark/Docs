cd6ef095bae97fa1ec01228b46732915d85a3479
tilark <tilark.liu@gmail.com>
2017-04-27 23:40:06 +0800

    changeSomething
    运行某些东西

--- aspnetcore/fundamentals/configuration/sample/src/ConfigJson/Program.cs
+++ aspnetcore/fundamentals/configuration/sample/src/ConfigJson/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Text;
 
 // Add NuGet <package id="Microsoft.Extensions.Configuration" and
 // <package id="Microsoft.Extensions.Configuration.Json"
@@ -12,15 +13,44 @@ public class Program
     public static IConfigurationRoot Configuration { get; set; }
     public static void Main(string[] args = null)
     {
+        //var builder = new ConfigurationBuilder()
+        //     .SetBasePath(Directory.GetCurrentDirectory())
+        //    .AddJsonFile("appsettings.json");
+
+        //Configuration = builder.Build();
+
+        //Console.WriteLine($"option1 = {Configuration["option1"]}");
+        //Console.WriteLine($"option2 = {Configuration["option2"]}");
+        //Console.WriteLine(
+        //    $"option1 = {Configuration["subsection:suboption1"]}");
+        CreateFileDirectory(new string[] { "test1", "Test2" });
+    }
+
+    private static void CreateFileDirectory(string[] paramsString)
+    {
         var builder = new ConfigurationBuilder()
              .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json");
 
         Configuration = builder.Build();
+        var result = new StringBuilder(200);
+        var fileDirectory =  Configuration["UploadFileDirectory:UploadTemplateDirectory"];
+        var uploadFilePath = Directory.GetCurrentDirectory() + $@"{fileDirectory}";
+        result.Append(uploadFilePath);
+        foreach (var param in paramsString)
+        {
+            result.AppendFormat("\\" + param);
+        }
+        Console.WriteLine( result.ToString().TrimEnd('\\'));
+    }
 
-        Console.WriteLine($"option1 = {Configuration["option1"]}");
-        Console.WriteLine($"option2 = {Configuration["option2"]}");
-        Console.WriteLine(
-            $"option1 = {Configuration["subsection:suboption1"]}");
+    public class FileUploadConfig
+    {
+        public FileUploadConfig()
+        {
+
+        }
+        public string UploadTemplateDirectory { get; set; }
+        public string UploadDirectory { get; set; }
     }
 }
